52f7a949ee20e487f9868a33a57848900b277545
Sergey <fejanin@mail.ru>
2024-07-06 14:06:47 +0300

    Added the display of a single post. Added slug navigation using get_absolute_url. Pagination has been added.

--- poll/blog/models.py
+++ poll/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -14,7 +15,9 @@ class Post(models.Model):
         DRAFT = 'DF', 'Черновик'
         PUBLISHED = 'PB', 'Опубликована'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            # слаг должен быть уникальным при равных полях publish
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -37,3 +40,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
--- poll/blog/views.py
+++ poll/blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # постраничная разбивка
+    paginator = Paginator(post_list, 3)  # кол-во объектов
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # если page_number не цлое число, то вернуть первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # если вызванная страница находится за пределами существующего диапазона,
+        # то вернуть последнюю страницу
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=slug,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
