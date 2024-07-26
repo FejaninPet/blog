f89143f9701f7f9b7fcfce901a2d707fb2fb4879
Sergey <fejanin@mail.ru>
2024-07-26 19:02:21 +0300

    Creating a search using a vector and a weight.

--- poll/blog/views.py
+++ poll/blog/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def post_list(request, tag_slug=None):
@@ -96,3 +97,31 @@ def post_comment(request, post_id):
         comment.save()
     return render(request, 'blog/post/comment.html',
                   {'post': post, 'form': form, 'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            # создание поиска с весами
+            # A - 1.0; B - 0.4; C - 0.2; D - 0.1
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A', config='russian') + \
+                            SearchVector('body', weight='B', config='russian')
+            search_query = SearchQuery(query, config='russian')
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {
+                      'form': form,
+                      'query': query,
+                      'results': results
+                  })
